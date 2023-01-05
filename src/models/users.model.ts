c5616fbc0e5ec4230c50123920191c199289ff5c
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2023-01-05 12:54:22 +0100

    feat: created user model

--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -1,18 +1,97 @@
-import { model, Schema, Document } from 'mongoose';
-import { User } from '@interfaces/users.interface';
+import { model, Schema, ObjectId } from 'mongoose';
+import bcrypt from 'bcrypt';
+import { IUserDoc, IUserModel } from '@interfaces/users.interface';
+import { toJSON } from '@/modules/toJSON';
+import { paginate } from '@/modules/paginate';
 
-const userSchema: Schema = new Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new Schema<IUserDoc, IUserModel>(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+    pocketTag: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    kycStatus: {
+      type: Boolean,
+      default: false,
+    },
+    kybStatus: {
+      type: Boolean,
+      default: false,
+    },
+    businessName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
+  {
+    timestamps: true,
   },
+);
+
+// add plugin that converts mongoose to json
+userSchema.plugin(toJSON);
+userSchema.plugin(paginate);
+
+/**
+ * Check if email is taken
+ * @param {string} email - The user's email
+ * @param {ObjectId} [excludeUserId] - The id of the user to be excluded
+ * @returns {Promise<boolean>}
+ */
+userSchema.static('isEmailTaken', async function (email: string, excludeUserId: ObjectId): Promise<boolean> {
+  const user = await this.findOne({ email, _id: { $ne: excludeUserId } });
+  return !!user;
 });
 
-const userModel = model<User & Document>('User', userSchema);
+/**
+ * Check if password matches the user's password
+ * @param {string} password
+ * @returns {Promise<boolean>}
+ */
+userSchema.method('isPasswordMatch', async function (password: string): Promise<boolean> {
+  const user = this;
+  return bcrypt.compare(password, user.password);
+});
 
-export default userModel;
+userSchema.pre('save', async function (next) {
+  const user = this;
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
+
+const User = model<IUserDoc, IUserModel>('User', userSchema);
+
+export default User;
