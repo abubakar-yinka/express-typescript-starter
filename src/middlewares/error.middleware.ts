a5fd25b13f09d77bdb1d3b6f51e670e0ca38a43a
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2023-01-05 12:42:14 +0100

    refactor: converted error middleware into a class and modified its logic

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -2,16 +2,34 @@ import { NextFunction, Request, Response } from 'express';
 import { HttpException } from '@exceptions/HttpException';
 import { logger } from '@utils/logger';
 
-const errorMiddleware = (error: HttpException, req: Request, res: Response, next: NextFunction) => {
-  try {
-    const status: number = error.status || 500;
-    const message: string = error.message || 'Something went wrong';
+export default class ErrorMiddleware {
+  static handleError = () => {
+    return async (error: HttpException, req: Request, res: Response, next: NextFunction) => {
+      try {
+        const status: number = error.statusCode || 500;
+        const message: string = error.message || 'Something went wrong';
+        const rawErrors: string[] = error.rawErrors || [];
+        // const stack: string = error.stack || 'No stack trace available';
 
-    logger.error(`[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}`);
-    res.status(status).json({ message });
-  } catch (error) {
-    next(error);
-  }
-};
+        logger.error(`[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}`);
+        res.status(status).json({ message, rawErrors });
+      } catch (error) {
+        next(error);
+      }
+    };
+  };
 
-export default errorMiddleware;
+  static initializeUnhandledException = () => {
+    process.on('unhandledRejection', (reason: Error) => {
+      logger.error(`[${reason.name}] >> ${reason.message}`);
+      logger.info('UNHANDLED REJECTION! 💥 Shutting down...');
+      throw reason;
+    });
+
+    process.on('uncaughtException', (error: Error) => {
+      logger.error(`[${error.name}] >> ${error.message}`);
+      logger.info('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+      process.exit(1);
+    });
+  };
+}
