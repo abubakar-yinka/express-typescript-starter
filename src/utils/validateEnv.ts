38ce297b9420765953fcb6df9f3545040426c3dd
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2023-01-05 13:50:34 +0100

    chore: modified the validateEnv function

--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -2,7 +2,7 @@ import { cleanEnv, port, str } from 'envalid';
 
 const validateEnv = () => {
   cleanEnv(process.env, {
-    NODE_ENV: str(),
+    NODE_ENV: str({ choices: ['development', 'test', 'production', 'staging'] }),
     PORT: port(),
   });
 };
