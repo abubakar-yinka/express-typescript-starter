e3111331dd553b86ac2963f7d295f0b0bcc12264
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2023-01-05 13:49:43 +0100

    refactor: converted logger function to a class

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,65 +1,109 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { existsSync, mkdirSync } from 'fs';
 import { join } from 'path';
-import winston from 'winston';
+import { format, transports, createLogger } from 'winston';
 import winstonDaily from 'winston-daily-rotate-file';
-import { LOG_DIR } from '@config';
+import { LOG_DIR, LOG_FORMAT, NODE_ENV } from '@config';
+import morgan, { StreamOptions } from 'morgan';
+
+const { combine, timestamp, splat, colorize } = format;
 
 // logs dir
-const logDir: string = join(__dirname, LOG_DIR);
+const logDir: string = join(__dirname, LOG_DIR!);
 
 if (!existsSync(logDir)) {
   mkdirSync(logDir);
 }
 
-// Define log format
-const logFormat = winston.format.printf(({ timestamp, level, message }) => `${timestamp} ${level}: ${message}`);
-
 /*
  * Log Level
  * error: 0, warn: 1, info: 2, http: 3, verbose: 4, debug: 5, silly: 6
  */
-const logger = winston.createLogger({
-  format: winston.format.combine(
-    winston.format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss',
-    }),
-    logFormat,
-  ),
-  transports: [
-    // debug log setting
-    new winstonDaily({
-      level: 'debug',
-      datePattern: 'YYYY-MM-DD',
-      dirname: logDir + '/debug', // log file /logs/debug/*.log in save
-      filename: `%DATE%.log`,
-      maxFiles: 30, // 30 Days saved
-      json: false,
-      zippedArchive: true,
-    }),
-    // error log setting
-    new winstonDaily({
+
+export class Logger {
+  static getInstance = (service = 'general-purpose') => {
+    const logger = createLogger({
+      defaultMeta: { service },
+      format: combine(
+        timestamp({
+          format: 'YYYY-MM-DD HH:mm:ss',
+        }),
+        Logger.logFormat,
+      ),
+      transports: [
+        // debug log setting
+        Logger.getDebugLoggerTransport(),
+        // error log setting
+        Logger.getErrorLoggerTransport(),
+      ],
+      exceptionHandlers: [new transports.File({ filename: 'exceptions.log' })],
+      rejectionHandlers: [new transports.File({ filename: 'rejections.log' })],
+    });
+
+    if (NODE_ENV !== 'production') {
+      logger.add(
+        new transports.Console({
+          format: combine(splat(), colorize()),
+        }),
+      );
+    }
+
+    return logger;
+  };
+
+  static logFormat = format.printf(({ timestamp, level, message }) => `${timestamp} ${level}: ${message}`);
+
+  static info = (message: string) => {
+    Logger.info(message);
+  };
+
+  static error = (message: string) => {
+    Logger.error(message);
+  };
+
+  static getErrorLoggerTransport = () => {
+    return new winstonDaily({
       level: 'error',
       datePattern: 'YYYY-MM-DD',
       dirname: logDir + '/error', // log file /logs/error/*.log in save
-      filename: `%DATE%.log`,
+      filename: `info-%DATE%.log`,
       maxFiles: 30, // 30 Days saved
       handleExceptions: true,
       json: false,
       zippedArchive: true,
-    }),
-  ],
-});
+    });
+  };
 
-logger.add(
-  new winston.transports.Console({
-    format: winston.format.combine(winston.format.splat(), winston.format.colorize()),
-  }),
-);
+  static getDebugLoggerTransport = () => {
+    return new winstonDaily({
+      level: 'debug',
+      datePattern: 'YYYY-MM-DD',
+      dirname: logDir + '/debug', // log file /logs/debug/*.log in save
+      filename: `info-%DATE%.log`,
+      maxFiles: 30, // 30 Days saved
+      json: false,
+      zippedArchive: true,
+    });
+  };
 
-const stream = {
-  write: (message: string) => {
-    logger.info(message.substring(0, message.lastIndexOf('\n')));
-  },
-};
+  static getHttpLoggerInstance = () => {
+    const logger = Logger.getInstance();
 
-export { logger, stream };
+    const stream: StreamOptions = {
+      write: (message: string) => {
+        logger.info(message.substring(0, message.lastIndexOf('\n')));
+      },
+    };
+
+    const skip = () => {
+      const env = NODE_ENV || 'development';
+      return env !== 'development';
+    };
+
+    const morganMiddleware = morgan(LOG_FORMAT ?? 'dev', { stream, skip });
+
+    return morganMiddleware;
+  };
+}
+
+export const logger = Logger.getInstance();
