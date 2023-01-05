74e2d8169e1dc23fc4803583967fb813da97a8e7
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2023-01-05 14:08:45 +0100

    chore: success response class created

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -17,3 +17,13 @@ export const isEmpty = (value: string | number | object): boolean => {
     return false;
   }
 };
+
+export class SuccessResponse {
+  statusCode: number;
+  data: any;
+
+  constructor(data: any, statusCode = 200) {
+    this.statusCode = statusCode;
+    this.data = data;
+  }
+}
