e0c213cac99609d3b13f62ff5034f9573caf74e8
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2023-01-05 12:34:53 +0100

    chore: added global error handling to catch uncaught exceptions

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,16 @@ import AuthRoute from '@routes/auth.route';
 import IndexRoute from '@routes/index.route';
 import UsersRoute from '@routes/users.route';
 import validateEnv from '@utils/validateEnv';
+import ErrorMiddleware from '@middlewares/error.middleware';
 
 validateEnv();
 
 const app = new App([new IndexRoute(), new UsersRoute(), new AuthRoute()]);
 
 app.listen();
+
+ErrorMiddleware.initializeUnhandledException();
+
+process.on('SIGTERM', () => {
+  console.info('SIGTERM received');
+});
