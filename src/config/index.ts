17a903d1ae1619e15da7afce15a1fcc9be8a3470
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2023-01-05 12:36:34 +0100

    chore: modified app config file

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,23 @@
 import { config } from 'dotenv';
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
-export const CREDENTIALS = process.env.CREDENTIALS === 'true';
-export const { NODE_ENV, PORT, DB_HOST, DB_PORT, DB_DATABASE, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN } = process.env;
+export const HAS_CREDENTIALS = process.env.CREDENTIALS === 'true';
+export const {
+  NODE_ENV,
+  PORT,
+  DB_HOST,
+  DB_PORT,
+  DB_DATABASE,
+  MONGODB_URI,
+  MONGODB_URI_DEV,
+  SECRET_KEY,
+  LOG_FORMAT,
+  LOG_DIR,
+  ORIGIN,
+  SMPT_HOST,
+  SMPT_PORT,
+  SMPT_SERVICE,
+  MAIL_USERNAME,
+  MAIL_PASSWORD,
+  CLIENT_URL,
+} = process.env;
