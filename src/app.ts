35ea917b67ceb5c7f5749f69400e547ce94ddc52
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2023-01-05 12:34:11 +0100

    chore: modified main app class

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,23 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import compression from 'compression';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
 import helmet from 'helmet';
 import hpp from 'hpp';
-import morgan from 'morgan';
 import { connect, set } from 'mongoose';
 import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
-import { NODE_ENV, PORT, LOG_FORMAT, ORIGIN, CREDENTIALS } from '@config';
+import ExpressMongoSanitize from 'express-mongo-sanitize';
+import tooBusy from 'toobusy-js';
+import xss from 'xss-clean';
+import { NODE_ENV, PORT, ORIGIN, HAS_CREDENTIALS } from '@config';
 import { dbConnection } from '@databases';
 import { Routes } from '@interfaces/routes.interface';
-import errorMiddleware from '@middlewares/error.middleware';
-import { logger, stream } from '@utils/logger';
+import ErrorMiddleware from '@middlewares/error.middleware';
+import { logger, Logger } from '@utils/logger';
+import { NotFoundError, ServiceUnavailableError } from '@exceptions/HttpException';
+// import rateLimiter from '@utils/rateLimiter';
 
 class App {
   public app: express.Application;
@@ -49,18 +54,35 @@ class App {
       set('debug', true);
     }
 
-    connect(dbConnection.url, dbConnection.options);
+    // @ts-ignore
+    connect(dbConnection.url, dbConnection.options)
+      .then(() => {
+        logger.info('Connected to Database Successfully...');
+      })
+      .catch(error => {
+        logger.error(`Error connecting to Database: ${error}`);
+      });
   }
 
   private initializeMiddlewares() {
-    this.app.use(morgan(LOG_FORMAT, { stream }));
-    this.app.use(cors({ origin: ORIGIN, credentials: CREDENTIALS }));
+    this.app.use(Logger.getHttpLoggerInstance());
+    this.app.use(cors({ origin: ORIGIN, credentials: HAS_CREDENTIALS }));
     this.app.use(hpp());
     this.app.use(helmet());
+    this.app.use(xss());
+    this.app.use(ExpressMongoSanitize());
     this.app.use(compression());
-    this.app.use(express.json());
+    this.app.use(express.json({ limit: '50kb' }));
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(cookieParser());
+    // this.app.use(rateLimiter);
+    this.app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+      if (tooBusy()) {
+        new ServiceUnavailableError('Server too busy!');
+      } else {
+        next();
+      }
+    });
   }
 
   private initializeRoutes(routes: Routes[]) {
@@ -75,7 +97,7 @@ class App {
         info: {
           title: 'REST API',
           version: '1.0.0',
-          description: 'Example docs',
+          description: 'API Documentation for the application',
         },
       },
       apis: ['swagger.yaml'],
@@ -86,7 +108,8 @@ class App {
   }
 
   private initializeErrorHandling() {
-    this.app.use(errorMiddleware);
+    this.app.use((req: express.Request, res: express.Response, next: express.NextFunction) => next(new NotFoundError(req.path)));
+    this.app.use(ErrorMiddleware.handleError());
   }
 }
 
