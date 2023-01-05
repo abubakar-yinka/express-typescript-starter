59f45599f6dd7df41aebb86489766140272d57e4
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2023-01-05 12:37:16 +0100

    chore: modified index controller

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -3,7 +3,12 @@ import { NextFunction, Request, Response } from 'express';
 class IndexController {
   public index = (req: Request, res: Response, next: NextFunction) => {
     try {
-      res.sendStatus(200);
+      res.status(200).json({
+        msg: 'Hello from App server',
+        Time: new Date(),
+        status: 'running',
+        server: 'Express + TS Server',
+      });
     } catch (error) {
       next(error);
     }
