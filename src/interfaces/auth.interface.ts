67d22b2cb1f1a20526603a58de9d077876964550
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2023-01-05 12:43:09 +0100

    chore: modified user interface

--- src/interfaces/auth.interface.ts
+++ src/interfaces/auth.interface.ts
@@ -1,5 +1,5 @@
 import { Request } from 'express';
-import { User } from '@interfaces/users.interface';
+import { IUser } from '@interfaces/users.interface';
 
 export interface DataStoredInToken {
   _id: string;
@@ -11,5 +11,5 @@ export interface TokenData {
 }
 
 export interface RequestWithUser extends Request {
-  user: User;
+  user: IUser;
 }
--- src/interfaces/users.interface.ts
+++ src/interfaces/users.interface.ts
@@ -1,5 +1,24 @@
-export interface User {
-  _id: string;
+import mongoose, { Model, Document } from 'mongoose';
+import { QueryResult } from '@/modules/paginate/paginate';
+
+export interface IUser {
+  firstName: string;
+  lastName: string;
   email: string;
+  phoneNumber: string;
   password: string;
+  pocketTag: string;
+  isEmailVerified: boolean;
+  kycStatus: boolean;
+  kybStatus: boolean;
+  businessName: string;
+}
+
+export interface IUserDoc extends IUser, Document {
+  isPasswordMatch(password: string): Promise<boolean>;
+}
+
+export interface IUserModel extends Model<IUserDoc> {
+  isEmailTaken(email: string, excludeUserId?: mongoose.Types.ObjectId): Promise<boolean>;
+  paginate(filter: Record<string, any>, options: Record<string, any>): Promise<QueryResult>;
 }
