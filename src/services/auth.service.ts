b7e41e3a57810a6d16ae4e6c774e5602aa64d983
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2023-01-05 14:06:55 +0100

    chore: auth service refactored

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -4,29 +4,29 @@ import { SECRET_KEY } from '@config';
 import { CreateUserDto } from '@dtos/users.dto';
 import { HttpException } from '@exceptions/HttpException';
 import { DataStoredInToken, TokenData } from '@interfaces/auth.interface';
-import { User } from '@interfaces/users.interface';
-import userModel from '@models/users.model';
+import { IUserDoc } from '@interfaces/users.interface';
+import User from '@models/users.model';
 import { isEmpty } from '@utils/util';
 
 class AuthService {
-  public users = userModel;
+  public users = User;
 
-  public async signup(userData: CreateUserDto): Promise<User> {
+  public async signup(userData: CreateUserDto): Promise<IUserDoc> {
     if (isEmpty(userData)) throw new HttpException(400, "You're not userData");
 
-    const findUser: User = await this.users.findOne({ email: userData.email });
+    const findUser: IUserDoc | null = await this.users.findOne({ email: userData.email });
     if (findUser) throw new HttpException(409, `You're email ${userData.email} already exists`);
 
     const hashedPassword = await hash(userData.password, 10);
-    const createUserData: User = await this.users.create({ ...userData, password: hashedPassword });
+    const createUserData: IUserDoc = await this.users.create({ ...userData, password: hashedPassword });
 
     return createUserData;
   }
 
-  public async login(userData: CreateUserDto): Promise<{ cookie: string; findUser: User }> {
+  public async login(userData: CreateUserDto): Promise<{ cookie: string; findUser: IUserDoc }> {
     if (isEmpty(userData)) throw new HttpException(400, "You're not userData");
 
-    const findUser: User = await this.users.findOne({ email: userData.email });
+    const findUser: IUserDoc | null = await this.users.findOne({ email: userData.email });
     if (!findUser) throw new HttpException(409, `You're email ${userData.email} not found`);
 
     const isPasswordMatching: boolean = await compare(userData.password, findUser.password);
@@ -38,21 +38,22 @@ class AuthService {
     return { cookie, findUser };
   }
 
-  public async logout(userData: User): Promise<User> {
+  public async logout(userData: IUserDoc): Promise<IUserDoc> {
     if (isEmpty(userData)) throw new HttpException(400, "You're not userData");
 
-    const findUser: User = await this.users.findOne({ email: userData.email, password: userData.password });
+    const findUser: IUserDoc | null = await this.users.findOne({ email: userData.email, password: userData.password });
     if (!findUser) throw new HttpException(409, `You're email ${userData.email} not found`);
 
     return findUser;
   }
 
-  public createToken(user: User): TokenData {
+  public createToken(user: IUserDoc): TokenData {
     const dataStoredInToken: DataStoredInToken = { _id: user._id };
-    const secretKey: string = SECRET_KEY;
+    const secretKey: string | undefined = SECRET_KEY;
     const expiresIn: number = 60 * 60;
 
-    return { expiresIn, token: sign(dataStoredInToken, secretKey, { expiresIn }) };
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    return { expiresIn, token: sign(dataStoredInToken, secretKey!, { expiresIn }) };
   }
 
   public createCookie(tokenData: TokenData): string {
