3b8899bd3a47d54eeb91bcd10bec9e211b329c11
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2023-01-05 12:38:09 +0100

    choreL modified DB configuration

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -1,10 +1,11 @@
-import { DB_HOST, DB_PORT, DB_DATABASE } from '@config';
+import { DB_HOST, DB_PORT, DB_DATABASE, MONGODB_URI, MONGODB_URI_DEV } from '@config';
+
+const connectionString = process.env.NODE_ENV === 'production' ? MONGODB_URI : MONGODB_URI_DEV;
 
 export const dbConnection = {
-  url: `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`,
+  url: connectionString ?? `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`,
   options: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useFindAndModify: false,
   },
 };
