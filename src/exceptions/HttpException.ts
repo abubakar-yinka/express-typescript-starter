be2b5ad0a6a24c21be8c9629cfa2eb08f53d1dae
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2023-01-05 12:41:03 +0100

    feat: implemented custom error classes

--- src/exceptions/HttpException.ts
+++ src/exceptions/HttpException.ts
@@ -1,10 +1,40 @@
-export class HttpException extends Error {
-  public status: number;
-  public message: string;
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { StatusCodes } from 'http-status-codes';
 
-  constructor(status: number, message: string) {
+export class HttpException extends Error {
+  public statusCode: number;
+  rawErrors: string[] = [];
+
+  constructor(statusCode: number, message: string, rawErrors?: string[]) {
     super(message);
-    this.status = status;
-    this.message = message;
+    this.statusCode = statusCode;
+    if (rawErrors) this.rawErrors = rawErrors;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export class NotFoundError extends HttpException {
+  constructor(path: string) {
+    super(StatusCodes.NOT_FOUND, `The requested path ${path} not found!`);
+  }
+}
+
+export class BadRequestError extends HttpException {
+  constructor(message: string, errors: string[]) {
+    super(StatusCodes.BAD_REQUEST, message, errors);
+  }
+}
+
+export class ApplicationError extends HttpException {
+  constructor(message: string, errors?: string[]) {
+    super(StatusCodes.INTERNAL_SERVER_ERROR, message, errors);
+  }
+}
+
+export class ServiceUnavailableError extends HttpException {
+  constructor(message: string, errors?: string[]) {
+    super(StatusCodes.SERVICE_UNAVAILABLE, message, errors);
   }
 }
